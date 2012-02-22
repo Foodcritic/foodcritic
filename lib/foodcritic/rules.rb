32203f107f3e62b391824edaf631164d0f26bfc6
Artem <artem@veremey.net>
2012-02-22 11:32:28 -0800

    Updating the rule to treat 0xxx modes as valid

--- lib/foodcritic/rules.rb
+++ lib/foodcritic/rules.rb
@@ -48,7 +48,7 @@ end
 rule "FC006", "Mode should be quoted or fully specified when setting file permissions" do
   tags %w{correctness files}
   recipe do |ast|
-    ast.xpath(%q{//ident[@value='mode']/parent::command/descendant::int[string-length(@value) < 5]/
+    ast.xpath(%q{//ident[@value='mode']/parent::command/descendant::int[string-length(@value) < 5 and not(starts-with(@value, "0") and string-length(@value) = 4)]/
       ancestor::method_add_block}).map{|resource| match(resource)}
   end
 end
