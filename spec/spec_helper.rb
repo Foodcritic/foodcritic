b2618677025edb82f4885bcabf28465521c6436e
Andrew Crump <andrew.crump@ieee.org>
2012-03-03 12:27:26 +0000

    Calculate unit test coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
-require_relative '../lib/foodcritic'
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
 
-require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/spec'
+
+require_relative '../lib/foodcritic'
