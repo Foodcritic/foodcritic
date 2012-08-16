ee701b0fc3f56f54e7dd3f5cf8beca9efcde1d4f
Dana Merrick <dmerrick@brightcove.com>
2012-08-16 15:57:07 -0400

    Adding real logic for the foodcritic ignore file.

--- lib/foodcritic/domain.rb
+++ lib/foodcritic/domain.rb
@@ -19,7 +19,7 @@ module FoodCritic
 
     attr_reader :cookbook_paths, :warnings
 
-    def initialize(cookbook_paths, warnings, is_failed, ignore_file)
+    def initialize(cookbook_paths, warnings, is_failed, ignore_file = nil)
       @cookbook_paths = Array(cookbook_paths)
       @warnings = warnings
       @is_failed = is_failed
@@ -29,8 +29,21 @@ module FoodCritic
     # Returns a list of warnings where those that are listed in an ignore file
     # are omitted.
     def quieter_warnings
-      # silly modification to test the command line logic
-      [@warnings.first]
+      # Remove any warnings that match the ignore rules
+      @warnings.reject do |w|
+        ignore_rules.any? do |code, file, line|
+
+          # If no line is provided in the ignore file, use the warning's line
+          # This will ignore the rule across the entire file
+          line = line ? line.to_i : w.match[:line]
+
+          same_code = code == w.rule.code
+          same_file = file == w.match[:filename]
+          same_line = line == w.match[:line]
+
+          same_code && same_file && same_line
+        end
+      end
     end
 
     # Provided for backwards compatibility. Deprecated and will be removed in a
@@ -57,6 +70,24 @@ module FoodCritic
       end.sort do |x,y|
         x.first == y.first ? x[1] <=> y[1] : x.first <=> y.first
       end.map{|w|"#{w.first}:#{w[1]}"}.uniq.join("\n")
+    end
+
+    private
+
+    # Returns a multi-dimensional array of warnings to ignore in the review.
+    #
+    #     [["FC004", "cookbooks/foo/recipes/default.rb", "22"],
+    #      ["FC014", "cookbooks/bar/recipes/qux.rb", "37"],
+    #      ["FC017", "cookbooks/baz/providers/quux.rb", "1"]]
+    def ignore_rules
+      # Read the ignore file
+      ignore = File.open(@ignore_file).readlines
+      # Strip out comments and blank lines
+      ignore.reject! {|line| line !~ /^FC/}
+      # Split the line into its component parts
+      ignore.map! {|rule| rule.chomp.split(/: ?/)}
+      # Throw away the rule name
+      ignore.each {|rule| rule.delete_at(1)}
     end
   end
 
