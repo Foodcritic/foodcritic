a34fbedd5c5a33d74dfa379b00038c6616b9d794
Andrew Crump <andrew.crump@ieee.org>
2013-11-12 21:01:04 +0000

    FC034: Allow for template platform differences.
    If a template resource may be rendered using one of several templates
    based on the platform then we don't want to warn with FC034 unless all
    of the templates have unused variables.

--- lib/foodcritic/rules.rb
+++ lib/foodcritic/rules.rb
@@ -492,25 +492,27 @@ rule "FC034", "Unused template variables" do
       t['variables'] and t['variables'].respond_to?(:xpath)
     end.map do |resource|
       all_templates = template_paths(filename)
-      template_path = all_templates.find do |path|
+      template_paths = all_templates.select do |path|
         File.basename(path) == template_file(resource)
       end
-      next unless template_path
+      next unless template_paths.any?
       passed_vars = resource['variables'].xpath(
         'symbol/ident/@value').map{|tv| tv.to_s}
 
-      begin
-        template_vars = templates_included(
-          all_templates, template_path).map do |template|
-            read_ast(template).xpath('//var_ref/ivar/@value').map do |v|
-              v.to_s.sub(/^@/, '')
-            end
-        end.flatten
-
-        file_match(template_path) unless (passed_vars - template_vars).empty?
-      rescue RecursedTooFarError
-        []
+      unused_vars_exist = template_paths.all? do |template_path|
+        begin
+          template_vars = templates_included(
+            all_templates, template_path).map do |template|
+              read_ast(template).xpath('//var_ref/ivar/@value').map do |v|
+                v.to_s.sub(/^@/, '')
+              end
+          end.flatten
+          ! (passed_vars - template_vars).empty?
+        rescue RecursedTooFarError
+          false
+        end
       end
+      file_match(template_paths.first) if unused_vars_exist
     end.compact
   end
 end
