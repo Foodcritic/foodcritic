57bcd2c2d1977f86024c0129f2c26d30c472feb7
Chaz Ruhl <chazzly@gmail.com>
2014-04-23 12:17:16 -0700

    Update linter.rb
    pull in and combine multiple .foodcritic files:

--- lib/foodcritic/linter.rb
+++ lib/foodcritic/linter.rb
@@ -199,13 +199,20 @@ module FoodCritic
 
     def cookbook_tags(file)
       tags = []
-      fc_file = "#{cookbook_dir(file)}/.foodcritic"
-      if File.exist? fc_file
-        begin
-          tag_text = File.read fc_file
-          tags = tag_text.split(/\s/)
-        rescue Errno::EACCES
-        end
+      fc_files = [
+        "#{cookbook_dir(file)}/.foodcritic",
+        Pathname.new('~/.foodcritic').expand_path.to_s,
+        Pathname.new('~/.chef/.foodcritic').expand_path.to_s,
+        Pathname.new( cookbook_dir(file)).parent.parent.to_s+"/config/.foodcritic"
+      ]
+      fc_files.each do |fc_file|
+       if File.exist? fc_file
+         begin
+           tag_text = File.read fc_file
+           tags.concat ( tag_text.split(/\s/) )
+         rescue Errno::EACCES
+         end
+       end
       end
       tags
     end
