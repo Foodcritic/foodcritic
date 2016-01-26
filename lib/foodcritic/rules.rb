4e28673e6f76f1c35b5fbc882c2ebc779adfa1ef
Nathen Harvey <nharvey@chef.io>
2016-01-25 22:40:07 -0500

    Add FC065 to the command_helpers

--- lib/foodcritic/rules.rb
+++ lib/foodcritic/rules.rb
@@ -868,3 +868,10 @@ rule 'FC064', 'Ensure issues_url is set in metadata' do
     [file_match(filename)] unless field(ast, 'issues_url').any?
   end
 end
+
+rule 'FC065', 'Ensure source_url is set in metadata' do
+  tags %w(metadata supermarket)
+  metadata do |ast, filename|
+    [file_match(filename)] unless field(ast, 'source_url').any?
+  end
+end
