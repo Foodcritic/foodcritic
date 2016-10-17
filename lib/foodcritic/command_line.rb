dc2f549bee8e9028b37bd59e856e85616b4da004
chazzly <chazzly@gmail.com>
2016-10-17 11:58:55 -0500

    rubocop fixes

--- lib/foodcritic/command_line.rb
+++ lib/foodcritic/command_line.rb
@@ -4,7 +4,7 @@ module FoodCritic
     # Create a new instance of CommandLine
     #
     # @param [Array] args The command line arguments
-    require 'optparse'
+    require "optparse"
 
     def initialize(args)
       # Load config_files first, so command line will override
@@ -93,17 +93,17 @@ module FoodCritic
 
       get_config(cbs)
 
-      @options.merge!(fail_tags: @fc_config['fail_tags'].flatten.uniq) if @fc_config['fail_tags']
-      @options.merge!(tags: @fc_config['tags'].flatten.uniq) if @fc_config['tags']
-      @options.merge!(include_rules: @fc_config['include_rules'].flatten.uniq) if @fc_config['include_rules']
-      @options.merge!(role_paths: @fc_config['role_paths'].flatten.uniq) if @fc_config['role_paths']
-      @options.merge!(environment_paths: @fc_config['environment_paths'].flatten.uniq) if @fc_config['environment_paths']
-      @options.merge!(exclude_paths: @fc_config['exclude_paths'].flatten.uniq) if @fc_config['exclude_paths']
-      @options.merge!(cookbook_paths: @fc_config['cookbook_paths'].flatten.uniq) if @fc_config['cookbook_paths']
-      @options.merge!(search_grammar: @fc_config['search_grammar']) if @fc_config['search_grammar']
-      @options.merge!(chef_version: @fc_config['chef_version']) if @fc_config['chef_version']
-      @options.merge!(context: @fc_config['context']) if @fc_config['context']
-      @options.merge!(search_gems: @fc_config['search_gems']) if @fc_config['search_gems']
+      @options.merge!(fail_tags: @fc_config["fail_tags"].flatten.uniq) if @fc_config["fail_tags"]
+      @options.merge!(tags: @fc_config["tags"].flatten.uniq) if @fc_config["tags"]
+      @options.merge!(include_rules: @fc_config["include_rules"].flatten.uniq) if @fc_config["include_rules"]
+      @options.merge!(role_paths: @fc_config["role_paths"].flatten.uniq) if @fc_config["role_paths"]
+      @options.merge!(environment_paths: @fc_config["environment_paths"].flatten.uniq) if @fc_config["environment_paths"]
+      @options.merge!(exclude_paths: @fc_config["exclude_paths"].flatten.uniq) if @fc_config["exclude_paths"]
+      @options.merge!(cookbook_paths: @fc_config["cookbook_paths"].flatten.uniq) if @fc_config["cookbook_paths"]
+      @options.merge!(search_grammar: @fc_config["search_grammar"]) if @fc_config["search_grammar"]
+      @options.merge!(chef_version: @fc_config["chef_version"]) if @fc_config["chef_version"]
+      @options.merge!(context: @fc_config["context"]) if @fc_config["context"]
+      @options.merge!(search_gems: @fc_config["search_gems"]) if @fc_config["search_gems"]
 
       # -v is not implemented but OptionParser gives the Foodcritic's version
       # if that flag is passed
@@ -118,7 +118,7 @@ module FoodCritic
       end
 
       [:cookbook_paths, :role_paths, :environment_paths, :include_rules, :exclude_paths].each do |pth|
-        @options[pth].map! { |c| File.expand_path(c, '.') } if @options[pth] && @options[pth] != [nil]
+        @options[pth].map! { |c| File.expand_path(c, ".") } if @options[pth] && @options[pth] != [nil]
         @options[pth].flatten!
         @options[pth].uniq!
       end
@@ -126,23 +126,23 @@ module FoodCritic
 
     # Search for and load config files
     def get_config(cbs)
-      require 'app_conf'
+      require "app_conf"
       files_to_load = []
-      files_to_load << File.expand_path('~/.chef/foodcritic.yml', '.') if File.file?(File.expand_path('~/.chef/foodcritic.yml'))
-      ['.', cbs].flatten.each do |mypth|
-        tmp_path = File.expand_path(mypth, '.')
-        until File.basename(tmp_path) == '/'
+      files_to_load << File.expand_path("~/.chef/foodcritic.yml", ".") if File.file?(File.expand_path("~/.chef/foodcritic.yml"))
+      [".", cbs].flatten.each do |mypth|
+        tmp_path = File.expand_path(mypth, ".")
+        until File.basename(tmp_path) == "/"
           files_to_load << "#{tmp_path}/.chef/foodcritic.yml" if File.file?("#{tmp_path}/.chef/foodcritic.yml")
-          tmp_path = File.expand_path("#{tmp_path}/..",'.')
+          tmp_path = File.expand_path("#{tmp_path}/..",".")
         end
       end
-      if @original_args.include?('--config')
-        ind = @original_args.index('--config') + 1
+      if @original_args.include?("--config")
+        ind = @original_args.index("--config") + 1
         tmp_path = @original_args[ind]
-        files_to_load << tmp_path if File.file?(tmp_path) && File.basename(tmp_path) == 'foodcritic.yml'
+        files_to_load << tmp_path if File.file?(tmp_path) && File.basename(tmp_path) == "foodcritic.yml"
         if File.directory?(tmp_path)
-          files_to_load << '#{tmp_path}/foodcritic.yml' if File.file?('#{tmp_path}/foodcritic.yml')
-          files_to_load << '#{tmp_path}/.chef/foodcritic.yml' if File.file?('#{tmp_path}/.chef/foodcritic.yml')
+          files_to_load << "#{tmp_path}/foodcritic.yml" if File.file?("#{tmp_path}/foodcritic.yml")
+          files_to_load << "#{tmp_path}/.chef/foodcritic.yml" if File.file?("#{tmp_path}/.chef/foodcritic.yml")
         end
       end
 
@@ -157,9 +157,9 @@ module FoodCritic
         @fc_config.merge!(tmp_config.to_hash) do |key, v1, v2|
           if v1 == v2
             v1
-          elsif %w(chef_version config search_grammar search_gems context).include? key
+          elsif %w{chef_version config search_grammar search_gems context}.include? key
             v2 || v1
-          elsif v1.class.name == 'Array'
+          elsif v1.class.name == "Array"
             v1 << v2 if v2
           else 
             v1.each_line.to_a << v2 if v2
@@ -168,15 +168,15 @@ module FoodCritic
       end
 
       @fc_config.keys.each do |k|
-        if %w(chef_version config search_grammar).include? k && !@fc_config[k].class.name == 'String'
+        if %w{chef_version config search_grammar}.include?(k) && !@fc_config[k].class.name == "String"
           @fc_config[k] =  @fc_config[k].to_s
-        elsif %w(search_gems context).include? k && !@fc_config[k].class.name == 'TrueClass' && !@fc_config[k].class.name == 'FalseClass'
+        elsif %w{search_gems context}.include?(k) && !@fc_config[k].class.name == "TrueClass" && !@fc_config[k].class.name == "FalseClass"
           @fc_config[k] = true if @fc_config[k]
-        elsif %w(tags fail_tags include_rules role_paths environment_paths exclude_paths cookbook_paths).include? k
+        elsif %w{tags fail_tags include_rules role_paths environment_paths exclude_paths cookbook_paths}.include?(k)
           @fc_config[k] =  Array(@fc_config[k])
-        end 
+        end
       end
-              
+
       @fc_config
     end
 
