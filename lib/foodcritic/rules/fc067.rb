430a478a0d69e83e620dc86bf8a8c64c7dbdf4a0
Tim Smith <tsmith@chef.io>
2017-03-31 13:55:29 -0700

    Add supermarket tag to FC067 and FC068
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc067.rb
+++ lib/foodcritic/rules/fc067.rb
@@ -1,5 +1,5 @@
 rule "FC067", "Ensure at least one platform supported in metadata" do
-  tags %w{metadata}
+  tags %w{metadata supermarket}
   metadata do |ast, filename|
     [file_match(filename)] unless field(ast, "supports").any?
   end
