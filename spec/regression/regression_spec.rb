f888c869cbff5c509ba8fc1c696f13b44dc1ddf8
Noah Kantrowitz <noah@coderanger.net>
2017-04-06 00:48:48 -0700

    Revamp the regression spec so it actually works again.

--- spec/regression/regression_spec.rb
+++ spec/regression/regression_spec.rb
@@ -1,54 +1,31 @@
 require "spec_helper"
-require "fileutils"
-require "pathname"
 
 describe "regression test" do
-
-  def lint_regression_cookbooks
-    working_dir = Pathname.new("tmp/regression")
-    FileUtils.mkdir_p(working_dir)
-
-    pinned_cookbooks("spec/regression/cookbooks.txt").each do |cbk|
-      clone_cookbook(working_dir, cbk)
-    end
-
-    lint_cookbooks(working_dir)
-  end
-
-  def pinned_cookbooks(path)
-    File.read(path).lines.map do |line|
+  let(:cookbooks_txt) do
+    path = File.expand_path("../cookbooks.txt", __FILE__)
+    IO.readlines(path).map do |line|
       name, ref = line.strip.split(":")
       { :name => name, :ref => ref }
     end
   end
 
-  def clone_cookbook(clone_path, cbk)
-    target_path = clone_path + cbk[:name]
-    unless Dir.exist?(target_path)
-      `git clone -q git://github.com/chef-cookbooks/#{cbk[:name]}.git #{target_path}`
-      raise "Unable to clone git://github.com/chef-cookbooks/#{cbk[:name]}.git" unless $?.success?
+  let(:expected_output) do
+    path = File.expand_path("../expected-output.txt", __FILE__)
+    IO.readlines(path)
+  end
+
+  command("#{File.expand_path("../../../bin/foodcritic", __FILE__)} .", allow_error: true)
+
+  before do
+    cookbooks_txt.each do |cbk|
+      command("git clone -q https://github.com/chef-cookbooks/#{cbk[:name]}.git")
+      command("git checkout -q #{cbk[:ref]}", cwd: "#{temp_path}/#{cbk[:name]}")
     end
-    `cd #{target_path} && git checkout -q #{cbk[:ref]}`
-    raise "Unable to checkout revision for #{cbk[:name]}" unless $?.success?
   end
 
-  def lint_cookbooks(cookbook_path)
-    result = `cd #{cookbook_path} && foodcritic .`
-    raise "Unable to lint #{cookbook_path}" unless $?.success?
-    result
+  it do
+    expected_output.each do |line|
+      expect(subject.stdout).to include line
+    end
   end
-
-
-  let(:expected_lint_output) do
-    File.read("spec/regression/expected-output.txt")
-  end
-
-  let(:actual_lint_output) do
-    lint_regression_cookbooks
-  end
-
-  it "should result in the expected matches against a pinned set of cookbooks" do
-    actual_lint_output.must_equal expected_lint_output
-  end
-
 end
