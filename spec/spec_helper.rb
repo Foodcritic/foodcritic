70f0f0d5a987a7966a8466f8ba21bc15b58a0286
Noah Kantrowitz <noah@coderanger.net>
2017-04-06 00:55:42 -0700

    Don’t run focus filter on CI.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,7 +53,7 @@ end
 RSpec.configure do |config|
   # Basic configuraiton
   config.run_all_when_everything_filtered = true
-  config.filter_run(:focus)
+  config.filter_run(:focus) unless ENV['CI']
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
