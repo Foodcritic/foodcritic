b5002626254eaf8accbd125ccfb5464040d1870f
Tim Smith <tsmith@chef.io>
2017-04-07 22:22:40 -0700

    Add FC072 for alerting on the attribute keyword in metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/functional/fc072_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe "FC072" do
+  context "with a cookbook with a metadata file that contains an attribute keyword" do
+    metadata_file "attribute 'something/something',\n    display_name: 'something tuneable',\n    description: 'This tunes something',\n    default: 'none'"
+    it { is_expected.to violate_rule("FC072") }
+  end
+
+  context "with a cookbook with a metadata file that doesn't contain an attribute keyword" do
+    metadata_file
+    it { is_expected.not_to violate_rule("FC072") }
+  end
+end
