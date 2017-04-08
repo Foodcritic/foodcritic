bc0c56b93b14d868f8e7449780df3a5c322085f5
Noah Kantrowitz <noah@coderanger.net>
2017-04-08 00:56:00 -0700

    Rework the regression tests so we can regenerate the expected output easily and to show some progress info.

--- spec/regression/regression_spec.rb
+++ spec/regression/regression_spec.rb
@@ -1,31 +1,23 @@
 require "spec_helper"
 
 describe "regression test" do
-  let(:cookbooks_txt) do
-    path = File.expand_path("../cookbooks.txt", __FILE__)
-    IO.readlines(path).map do |line|
-      name, ref = line.strip.split(":")
-      { :name => name, :ref => ref }
-    end
-  end
-
-  let(:expected_output) do
-    path = File.expand_path("../expected-output.txt", __FILE__)
-    IO.readlines(path)
-  end
-
   command("#{File.expand_path("../../../bin/foodcritic", __FILE__)} .", allow_error: true)
 
-  before do
-    cookbooks_txt.each do |cbk|
-      command("git clone -q https://github.com/chef-cookbooks/#{cbk[:name]}.git")
-      command("git checkout -q #{cbk[:ref]}", cwd: "#{temp_path}/#{cbk[:name]}")
-    end
-  end
+  IO.readlines(File.expand_path("../cookbooks.txt", __FILE__)).each do |line|
+    name, ref = line.strip.split(":")
 
-  it do
-    expected_output.each do |line|
-      expect(subject.stdout).to include line
+    context "with cookbook #{name}" do
+      before do
+        command("git clone -q https://github.com/chef-cookbooks/#{name}.git .")
+        command("git checkout -q #{ref}")
+      end
+
+      it "should match expected output" do
+        expected_output = IO.readlines(File.expand_path("../expected/#{name}.txt", __FILE__))
+        expected_output.each do |expected_line|
+          expect(subject.stdout).to include expected_line
+        end
+      end
     end
   end
 end
