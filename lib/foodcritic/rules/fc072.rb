001daadcb0e03e7c9a50a3fccb84c3385545c4e6
Tim Smith <tsmith@chef.io>
2017-04-08 13:31:37 -0700

    Regenerate regression test for FC072
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc072.rb
+++ lib/foodcritic/rules/fc072.rb
@@ -1,4 +1,4 @@
-rule "FC072", 'metadata should not contain "attribute" keyword' do
+rule "FC072", 'Metadata should not contain "attribute" keyword' do
   tags %w{metadata style}
   metadata do |ast, filename|
     [file_match(filename)] if field(ast, "attribute").any?
