cef9c629d995894484951b28b15cc89b73913775
Tim Smith <tsmith@chef.io>
2017-04-08 15:36:37 -0700

    Convert more cucumber to rspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/functional/fc052_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe "FC052" do
+  context "with a cookbook with a metadata file that does contain the suggests keyword" do
+    metadata_file "suggests 'runit'"
+    it { is_expected.to violate_rule("FC052") }
+  end
+
+  context "with a cookbook with a metadata file that does not contain the suggests keyword" do
+    metadata_file
+    it { is_expected.to_not violate_rule("FC052") }
+  end
+end
---
+++ spec/functional/fc062_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe "FC062" do
+  context "with a cookbook with metadata that includes the version keyword and a valid version string" do
+    metadata_file("version '1.2.3'")
+    it { is_expected.not_to violate_rule("FC062") }
+  end
+
+  context "with a cookbook with metadata that does not include a version keyword" do
+    metadata_file
+    it { is_expected.to violate_rule("FC062") }
+  end
+end
