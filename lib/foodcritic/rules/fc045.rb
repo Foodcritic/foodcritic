ab12a61831afae50a060d16bd67ec6db28517209
Tim Smith <tsmith@chef.io>
2017-04-08 23:11:01 -0700

    Simplify FC045 by using the field helper
    Also don’t fail if there isn’t metadata. We have a check for that already and there’s work in progress to fallback to metadata.json for metadata parsing.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc045.rb
+++ lib/foodcritic/rules/fc045.rb
@@ -1,13 +1,6 @@
 rule "FC045", "Metadata does not contain cookbook name" do
   tags %w{correctness metadata chef12}
   metadata do |ast, filename|
-    unless ast.xpath('descendant::stmts_add/command/ident/@value="name"')
-      [file_match(filename)]
-    end
-  end
-  cookbook do |filename|
-    if !File.exist?(File.join(filename, "metadata.rb"))
-      [file_match(File.join(filename, "metadata.rb"))]
-    end
+    [file_match(filename)] unless field(ast, "name").any?
   end
 end
