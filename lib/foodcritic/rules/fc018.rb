52be2ba33c13501bd8cc0183de1ae2abd646f450
Tim Smith <tsmith@chef.io>
2017-04-14 15:42:18 -0700

    Add tag chef13 to fc018
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc018.rb
+++ lib/foodcritic/rules/fc018.rb
@@ -1,5 +1,5 @@
 rule "FC018", "LWRP uses deprecated notification syntax" do
-  tags %w{correctness lwrp deprecated}
+  tags %w{correctness lwrp deprecated chef13}
   provider do |ast|
     ast.xpath("//assign/var_field/ivar[@value='@updated']").map do |class_var|
       match(class_var)
