5a18a08cb8e062e2b37cc232530f1e1ef4b37d0f
Tim Smith <tsmith@chef.io>
2017-04-15 20:56:52 -0700

    Convert more cucumber to rspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/functional/fc013_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+
+describe "FC013" do
+  context "with a cookbook that downloads a file to /tmp" do
+    recipe_file <<-EOH
+      remote_file "/tmp/large-file.tar.gz" do
+        source "http://www.example.org/large-file.tar.gz"
+      end
+    EOH
+    it { is_expected.to violate_rule("FC013") }
+  end
+
+  context "with a cookbook that downloads a file to /tmp with an expression" do
+    recipe_file <<-'EOH'
+      remote_file "/tmp/#{the_file}" do
+        source "http://www.example.org/large-file.tar.gz"
+      end
+    EOH
+    it { is_expected.to violate_rule("FC013") }
+  end
+
+  context "with a cookbook that downloads a file to a users home directory" do
+    recipe_file <<-EOH
+      remote_file "/home/ernie/large-file.tar.gz" do
+        source "http://www.example.org/large-file.tar.gz"
+      end
+    EOH
+    it { is_expected.not_to violate_rule("FC013") }
+  end
+
+  context "with a cookbook that downloads a file to the Chef file cache" do
+    recipe_file <<-'EOH'
+      remote_file '#{Chef::Config[:file_cache_path]}/large-file.tar.gz' do
+        source "http://www.example.org/large-file.tar.gz"
+      end
+    EOH
+    it { is_expected.not_to violate_rule("FC013") }
+  end
+end
