d97401fc9441e7bbc31188533a986338587b3658
Tim Smith <tsmith@chef.io>
2017-04-16 21:55:10 -0700

    Ignore case when checking for "All Rights Reserved" in FC071
    Updated version of #596 that adds a spec
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc071.rb
+++ lib/foodcritic/rules/fc071.rb
@@ -1,7 +1,7 @@
 rule "FC071", "Missing LICENSE file" do
   tags %w{style license}
   cookbook do |path|
-    unless ::File.exist?(File.join(path, "metadata.rb")) && field_value(read_ast(File.join(path, "metadata.rb")), "license") == "All Rights Reserved"
+    unless ::File.exist?(::File.join(path, "metadata.rb")) && field_value(read_ast(::File.join(path, "metadata.rb")), "license").to_s.casecmp("All Rights Reserved") == 0
       ensure_file_exists(path, "LICENSE")
     end
   end
