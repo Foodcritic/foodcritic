c74917d66cb6801b294ee095cd70e4b2b0881acf
Tim Smith <tsmith@chef.io>
2017-04-16 22:39:31 -0700

    Add FC079 to detect easy_package_install usage
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/functional/fc079_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe "FC079" do
+  context "with a cookbook with a recipe that includes an easy_install_package resource" do
+    recipe_file <<-EOF
+      easy_install_package "foo" do
+        action :install
+      end
+    EOF
+    it { is_expected.to violate_rule("FC079") }
+  end
+
+  context "with a cookbook with a recipe that doesn't include an easy_install_package resource" do
+    recipe_file <<-EOF
+      package "foo" do
+        action :install
+      end
+    EOF
+    it { is_expected.not_to violate_rule("FC079") }
+  end
+end
