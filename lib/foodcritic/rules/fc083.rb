16230ef17972f81bfbe15f1aeaf4c9f5febab775
Tim Smith <tsmith@chef.io>
2017-04-21 23:45:48 -0700

    Simplify rule code
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc083.rb
+++ lib/foodcritic/rules/fc083.rb
@@ -1,10 +1,6 @@
 rule "FC083", "Execute resource using 'path' property" do
   tags %w{deprecated chef12}
   recipe do |ast|
-    matches = []
-    find_resources(ast, type: "execute").find_all do |cmd|
-      matches << cmd.xpath(%q{descendant::fcall[ident/@value='path']|descendant::command[ident/@value='path']})
-    end
-    matches
+    find_resources(ast, type: "execute").xpath('(.//command|.//fcall)[ident/@value="path"]')
   end
 end
