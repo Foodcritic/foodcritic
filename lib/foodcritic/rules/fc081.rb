170440b05dcaeab2f98e743fe6ebbaa14753c05f
Tim Smith <tsmith@chef.io>
2017-04-21 22:02:29 -0700

    Rubocop fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc081.rb
+++ lib/foodcritic/rules/fc081.rb
@@ -1,6 +1,6 @@
 rule "FC081", "Cookbook depends on the partial_search cookbook" do
   tags %w{chef12}
   metadata do |ast, filename|
-    [file_match(filename)] if declared_dependencies(ast).include?('partial_search')
+    [file_match(filename)] if declared_dependencies(ast).include?("partial_search")
   end
 end
