8c71ef5277eea8c0c7639539d0aed5374badb0ee
Tim Smith <tsmith@chef.io>
2017-04-21 20:51:45 -0700

    cookbooks -> cookbook
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc081.rb
+++ lib/foodcritic/rules/fc081.rb
@@ -1,4 +1,4 @@
-rule "FC081", "Cookbook depends on the partial_search cookbooks" do
+rule "FC081", "Cookbook depends on the partial_search cookbook" do
   tags %w{chef12}
   recipe do |ast|
     [file_match('metadata.rb')] if declared_dependencies(ast).include?('partial_search')
