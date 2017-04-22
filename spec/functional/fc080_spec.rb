d31e9d870a0b81ffe9f9481f7a30a12a59025925
Tim Smith <tsmith@chef.io>
2017-04-21 17:59:38 -0700

    Add another test and update the rule to not catch supports in other resources
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/functional/fc080_spec.rb
+++ spec/functional/fc080_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "FC080" do
   context "with a cookbook with a recipe that includes a user resource using supports" do
     recipe_file <<-EOF
-      user "betty" do
+      user 'betty' do
         action :create
         supports({
           manage_home: true,
@@ -11,12 +11,12 @@ describe "FC080" do
         })
       end
     EOF
-    it { is_expected.to violate_rule("FC080") }
+    it { is_expected.to violate_rule }
   end
 
   context "with a cookbook with a recipe that includes a user resource using supports w/o parens" do
     recipe_file <<-EOF
-      user "betty" do
+      user 'betty' do
         action :create
         supports manage_home: true
       end
@@ -27,7 +27,7 @@ describe "FC080" do
   context "with a cookbook with a recipe that includes a user resource not using supports" do
     recipe_file <<-EOF
       user username do
-        shell "/bin/false"
+        shell '/bin/false'
         home  deploy_path
         system true
         action :create
@@ -38,9 +38,29 @@ describe "FC080" do
 
   context "with a cookbook with a recipe that includes a resource with supports" do
     recipe_file <<-EOF
-      service "foo" do
+      service 'foo' do
         action :start
         supports :restart
+    EOF
+    it { is_expected.not_to violate_rule }
+  end
+
+  context "with a cookbook with a recipe that includes a user resource and other supports uses" do
+    recipe_file <<-EOF
+      user 'foo' do
+        system true
+        action :create
+      end
+
+      def supports(thing)
+        puts thing
+      end
+
+      supports('something')
+
+      service 'foo' do
+        supports restart: true, status: true
+        action [:enable, :start]
       end
     EOF
     it { is_expected.not_to violate_rule }
