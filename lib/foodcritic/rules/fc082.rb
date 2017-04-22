bfc5088e66c2dfb1086998ad9573a64f30324e26
Tim Smith <tsmith@chef.io>
2017-04-21 21:40:54 -0700

    Catch node.set_unless as well
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc082.rb
+++ lib/foodcritic/rules/fc082.rb
@@ -1,8 +1,11 @@
-rule "FC082", "node.set used to set node attributes" do
+rule "FC082", "node.set or node.set_unless used to set node attributes" do
   tags %w{chef14 deprecated}
   def node_sets(ast)
+    # if someone wants to minimize this xpath query that'd be great
     ast.xpath('//call[(vcall|var_ref)/ident/@value="node"]
-    [ident/@value="set"]')
+    [ident/@value="set"] |
+    //call[(vcall|var_ref)/ident/@value="node"]
+    [ident/@value="set_unless"]')
   end
   recipe { |ast| node_sets(ast) }
   library { |ast| node_sets(ast) }
--- spec/functional/fc082_spec.rb
+++ spec/functional/fc082_spec.rb
@@ -3,7 +3,12 @@ require "spec_helper"
 describe "FC082" do
   context "with a cookbook with a recipe that sets an attribute with node.set" do
     recipe_file "node.set['foo']['bar'] = baz"
-    it { is_expected.to_not violate_rule }
+    it { is_expected.to violate_rule }
+  end
+
+  context "with a cookbook with a recipe that sets an attribute with node.set_unless" do
+    recipe_file "node.set_unless['foo']['bar'] = baz"
+    it { is_expected.to violate_rule }
   end
 
   context "with a cookbook with a recipe that sets an attribute with node.normal" do
