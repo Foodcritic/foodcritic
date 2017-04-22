f402ccab9f0e3666da3ed3815598cbb3f07a056d
Tim Smith <tsmith@chef.io>
2017-04-21 17:00:06 -0700

    Simplify a few specs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/functional/fc076_spec.rb
+++ spec/functional/fc076_spec.rb
@@ -3,11 +3,11 @@ require "spec_helper"
 describe "FC076" do
   context "with a cookbook with a metadata file that does contain the conflicts keyword" do
     metadata_file "conflicts 'runit'"
-    it { is_expected.to violate_rule("FC076") }
+    it { is_expected.to violate_rule }
   end
 
   context "with a cookbook with a metadata file that does not contain the conflicts keyword" do
     metadata_file
-    it { is_expected.to_not violate_rule("FC076") }
+    it { is_expected.to_not violate_rule }
   end
 end
