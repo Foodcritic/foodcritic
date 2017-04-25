5a9aecf8910b03bfe629b4a5be7218c8dddae838
Tim Smith <tsmith@chef.io>
2017-04-24 21:52:16 -0700

    Be more clear these things are bad because they’re deprecated
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc081.rb
+++ lib/foodcritic/rules/fc081.rb
@@ -1,5 +1,5 @@
-rule "FC081", "Cookbook depends on the partial_search cookbook" do
-  tags %w{chef12}
+rule "FC081", "Cookbook depends on the deprecated partial_search cookbook" do
+  tags %w{chef12 deprecated}
   metadata do |ast, filename|
     [file_match(filename)] if declared_dependencies(ast).include?("partial_search")
   end
