a2a462649cbe776ef803258a1b4d2aafd97a4d18
Tim Smith <tsmith@chef.io>
2017-05-18 13:23:22 -0700

    Add FC089 to detect usage of Chef::ShellOut
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/functional/fc089_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+describe "FC089" do
+  context "with a cookbook with a library that uses Chef::ShellOut" do
+    library_file "include Chef::ShellOut"
+    it { is_expected.to violate_rule }
+  end
+
+  context "with a cookbook with a resource that uses Chef::ShellOut" do
+    resource_file "include Chef::ShellOut"
+    it { is_expected.to violate_rule }
+  end
+
+  context "with a cookbook with a resource that uses Miblib::Shellout" do
+    resource_file "include Mixlib::Shellout"
+    it { is_expected.not_to violate_rule }
+  end
+end
