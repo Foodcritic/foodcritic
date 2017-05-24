e006ad032797ff86f9da1db739d115428721f7ca
Bharath Prakash <bharath.prakash@assurity.co.nz>
2017-05-25 09:59:27 +1200

    fixed the rubocop error

--- lib/foodcritic/rules/fc090.rb
+++ lib/foodcritic/rules/fc090.rb
@@ -6,7 +6,7 @@ rule "FC090", "Ensure supports metadata defines valid version format(x.y.z or x.
     metadata_platforms_versions = supported_platforms(ast).map { |x| x[:versions] }
     metadata_platforms_versions.each do |plat_vers|
       break if matches # we found one on the previous run so stop looking
-      modified_plat_vers = plat_vers.to_s.gsub(/[\"\[<>=~\] ]/,'')
+      modified_plat_vers = plat_vers.to_s.gsub(/[\"\[<>=~\] ]/,"")
       if (plat_vers && !plat_vers.empty?) && (modified_plat_vers !~ /\A\d+\.\d+(\.\d+)?\z/)
         matches = true
         break # stop looking
