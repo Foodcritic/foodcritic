6de8e55cac1a76a060fea215697d25f12731526e
Tim Smith <tsmith@chef.io>
2017-07-08 15:55:25 -0700

    Add some comments to FC033 code
    It's chained which makes it hard to follow
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc033.rb
+++ lib/foodcritic/rules/fc033.rb
@@ -1,14 +1,19 @@
 rule "FC033", "Missing template" do
   tags %w{correctness templates}
   recipe do |ast, filename|
+    # find all template resources that don't fetch a template
+    # from either another cookbook or a local path
     find_resources(ast, type: :template).reject do |resource|
       resource_attributes(resource)["local"] ||
         resource_attributes(resource)["cookbook"]
     end.map do |resource|
+      # fetch the specified file to the template
       file = template_file(resource_attributes(resource,
                                                return_expressions: true))
       { resource: resource, file: file }
     end.reject do |resource|
+      # skip the check if the file path is derived since
+      # we can't determine if that's here or not without converging the node
       resource[:file].respond_to?(:xpath)
     end.select do |resource|
       template_paths(filename).none? do |path|
