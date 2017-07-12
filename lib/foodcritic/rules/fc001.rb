b05f3d72822f43f97b852c6142b4b389001a7e5d
Tim Smith <tsmith@chef.io>
2017-07-12 11:41:43 -0700

    FC001 should not alert on node.run_state[:foo]
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc001.rb
+++ lib/foodcritic/rules/fc001.rb
@@ -2,6 +2,7 @@ rule "FC001",
      "Use strings in preference to symbols to access node attributes" do
   tags %w{style attributes}
   recipe do |ast|
-    attribute_access(ast, type: :symbol)
+    # node.run_state is not actually an attribute so ignore that
+    attribute_access(ast, type: :symbol, ignore: "run_state")
   end
 end
