e7bbdd27d8ed88d06378d1d14cc25bda07762bfe
John Bellone <jbellone@bloomberg.net>
2017-10-12 11:06:30 -0400

    Remove the correctness tag.

--- lib/foodcritic/rules/fc090.rb
+++ lib/foodcritic/rules/fc090.rb
@@ -1,5 +1,5 @@
 rule "FC090", "Resource should not ignore failures" do
-  tags %w{correctness resource}
+  tags %w{resource}
   recipe do |ast|
     find_resources(ast).select { |resource| resource_attributes(resource)["ignore_failure"] }.compact
   end
