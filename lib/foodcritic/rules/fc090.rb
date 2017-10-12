f83a8abea9bef932f921f86d7abd0accffe8f070
John Bellone <jbellone@bloomberg.net>
2017-10-12 11:05:50 -0400

    lint: Fix failures with single vs. double quote strings.

--- lib/foodcritic/rules/fc090.rb
+++ lib/foodcritic/rules/fc090.rb
@@ -1,6 +1,6 @@
 rule "FC090", "Resource should not ignore failures" do
   tags %w{correctness resource}
   recipe do |ast|
-    find_resources(ast).select { |resource| resource_attributes(resource)['ignore_failure'] }.compact
+    find_resources(ast).select { |resource| resource_attributes(resource)["ignore_failure"] }.compact
   end
 end
--- spec/functional/fc090_spec.rb
+++ spec/functional/fc090_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe 'FC090' do
-  context 'with a cookbook that installs package with ignore_failure set true' do
+describe "FC090" do
+  context "with a recipe that installs package with ignore_failure set true" do
     recipe_file <<-EOF
         package 'foo' do
           ignore_failure true
@@ -10,7 +10,7 @@ describe 'FC090' do
     it { is_expected.to violate_rule }
   end
 
-  context 'with a cookbook that installs package with ignore_failure set false' do
+  context "with a recipe that installs package with ignore_failure set false" do
     recipe_file <<-EOF
         package 'foo' do
           ignore_failure false
@@ -19,7 +19,7 @@ describe 'FC090' do
     it { is_expected.not_to violate_rule }
   end
 
-  context 'with a cookbook that installs package without defining ignore_failure' do
+  context "with a recipe that installs package without defining ignore_failure" do
     recipe_file "package 'foo'"
     it { is_expected.not_to violate_rule }
   end
