650aa09d342a5ed9a04b5dd9d4a6ce4e21609fcd
Tim Smith <tsmith@chef.io>
2017-10-19 16:47:45 -0700

    Expand FC087 to check for all deprecated Chef::Platform methods
    There's 3 deprecated methods and we deprecated them all at the same time. Let's check for all of them
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc087.rb
+++ lib/foodcritic/rules/fc087.rb
@@ -1,7 +1,7 @@
-rule "FC087", "Library maps provider with deprecated Chef::Platform.set" do
+rule "FC087", "Library uses deprecated Chef::Platform methods" do
   tags %w{chef13 deprecated}
 
   library do |ast|
-    ast.xpath('//const_path_ref/const[@value="Platform"]/..//const[@value="Chef"]/../../../ident[@value="set"]')
+    ast.xpath('//const_path_ref/const[@value="Platform"]/..//const[@value="Chef"]/../../../ident[@value="set" or @value="provider_for_resource" or @value="find_provider"]')
   end
 end
