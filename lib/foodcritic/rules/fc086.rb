c967c8d0f9bbf76fbfea177893167a5bc6d2f865
Tim Smith <tsmith@chef.io>
2017-11-13 12:16:16 -0800

    Fix FC086 to only alert when you try to load a databag
    Ignore the other methods in this class since there's valid uses for them within cookbook code
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc086.rb
+++ lib/foodcritic/rules/fc086.rb
@@ -1,6 +1,6 @@
 rule "FC086", "Use databag helper methods to load data bag items" do
   tags %w{style}
   recipe do |ast|
-    ast.xpath('//const_path_ref/const[@value="EncryptedDataBagItem" or @value="DataBagItem"]/..//const[@value="Chef"]/../../..//ident[@value!="load_secret"]/..')
+    ast.xpath('//const_path_ref/const[@value="EncryptedDataBagItem" or @value="DataBagItem"]/..//const[@value="Chef"]/../../..//ident[@value="load"]/..')
   end
 end
