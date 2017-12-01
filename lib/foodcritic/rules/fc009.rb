e55d9117dad69664f7fbb714c0bfa01840eadf2a
Tim Smith <tsmith@chef.io>
2017-12-01 13:24:46 -0800

    FC009 should not alert on a raise in a resource
    It's not the cleanest code, but it's not FC009's business
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc009.rb
+++ lib/foodcritic/rules/fc009.rb
@@ -5,7 +5,9 @@ rule "FC009", "Resource attribute not recognised" do
     resource_attributes_by_type(ast).each do |type, resources|
       resources.each do |resource|
         resource.keys.map(&:to_sym).reject do |att|
-          resource_attribute?(type.to_sym, att)
+          # look up the resource attribute give it's type (file, package, etc)
+          # raise is a special case. It's questionable, but shouldn't alert
+          resource_attribute?(type.to_sym, att) || att == :raise
         end.each do |invalid_att|
           matches << find_resources(ast, type: type).find do |res|
             resource_attributes(res).include?(invalid_att.to_s)
