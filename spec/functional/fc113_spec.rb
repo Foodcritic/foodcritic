4e6b620406727927be767dbb29f15798e63cecf3
Tim Smith <tsmith@chef.io>
2018-01-04 13:37:17 -0800

    Replace FC057/FC059 with FC113
    Don't alert when someone is missing use_inline_resources anymore. Alert when they use it instead.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/functional/fc113_spec.rb
+++ spec/functional/fc113_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "FC057" do
+describe "FC113" do
   context "with a cookbook with a library provider with use_inline_resources" do
     library_file <<-EOF
     class MyResources
@@ -24,7 +24,7 @@ describe "FC057" do
       end
     end
     EOF
-    it { is_expected.not_to violate_rule }
+    it { is_expected.to violate_rule }
   end
 
   context "with a cookbook with a library provider without use_inline_resources" do
@@ -48,13 +48,49 @@ describe "FC057" do
       end
     end
     EOF
-    it { is_expected.to violate_rule }
+    it { is_expected.not_to violate_rule }
   end
 
   context "with a cookbook with plain old library file" do
     library_file <<-EOF
     def something
       puts "I'm just a method"
+    end
+    EOF
+    it { is_expected.not_to violate_rule }
+  end
+
+  context "with a cookbook with LWRP not using use_inline_resources" do
+    provider_file <<-EOF
+    action :create do
+      template "/etc/something.conf" do
+        notifies :restart, "service[something]"
+      end
+    end
+    EOF
+    it { is_expected.not_to violate_rule }
+  end
+
+  context "with a cookbook with LWRP using use_inline_resources" do
+    provider_file <<-EOF
+    use_inline_resources
+
+    action :create do
+      template "/etc/something.conf" do
+        notifies :restart, "service[something]"
+      end
+    end
+    EOF
+    it { is_expected.to violate_rule }
+  end
+
+  context "with a cookbook ignoring the rule" do
+    provider_file <<-EOF.gsub(/^    /, "") # When we drop 2.2 support, this can use <<~EOF.
+    # ~FC059
+    action :create do
+      template "/etc/something.conf" do
+        notifies :restart, "service[something]"
+      end
     end
     EOF
     it { is_expected.not_to violate_rule }
