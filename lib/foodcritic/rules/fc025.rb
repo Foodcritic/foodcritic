a852403e2b914f50096793f157744c43032408c2
Tim Smith <tsmith@chef.io>
2018-01-19 14:17:04 -0800

    Fix FC025 for Ruby 2.5
    There's new elements and this is a VERY specific query which blows up
    with the extra elements. Hopefully this is the only one.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc025.rb
+++ lib/foodcritic/rules/fc025.rb
@@ -1,8 +1,10 @@
 rule "FC025", "Prefer chef_gem to compile-time gem install" do
   tags %w{correctness deprecated}
   recipe do |ast|
+    # when Ruby 2.4 support goes away this can be simplified to remove the
+    # do_block/stmts_add/command case which is Ruby 2.4
     gem_install = ast.xpath("//stmts_add/assign[method_add_block[command/ident/
-      @value='gem_package'][do_block/stmts_add/command[ident/@value='action']
+      @value='gem_package'][do_block/bodystmt/stmts_add/command[ident/@value='action'] | do_block/stmts_add/command[ident/@value='action']
       [descendant::ident/@value='nothing']]]")
     gem_install.map do |install|
       gem_var = install.xpath("var_field/ident/@value")
