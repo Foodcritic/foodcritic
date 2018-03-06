414e93fab32e56eedad1865795e8365a91f76c99
Tim Smith <tsmith@chef.io>
2018-03-06 13:47:32 -0800

    Take chef12 tags off issue_url and source_url
    People shouldn't think they need these to use Chef 12. They're just
    useful.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc064.rb
+++ lib/foodcritic/rules/fc064.rb
@@ -1,5 +1,5 @@
 rule "FC064", "Ensure issues_url is set in metadata" do
-  tags %w{metadata supermarket chef12}
+  tags %w{metadata supermarket}
   metadata do |ast, filename|
     [file_match(filename)] unless field(ast, "issues_url").any?
   end
