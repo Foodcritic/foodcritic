147e493d9e292b29fc7940a3641f783963f866ee
Tim Smith <tsmith@chef.io>
2018-05-11 13:28:32 -0700

    Add opensource tag to FC071
    If you're not opensource you really don't care about this rule.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc071.rb
+++ lib/foodcritic/rules/fc071.rb
@@ -1,5 +1,5 @@
 rule "FC071", "Missing LICENSE file" do
-  tags %w{style license}
+  tags %w{style license opensource}
   cookbook do |path|
     unless ::File.exist?(::File.join(path, "metadata.rb")) && field_value(read_ast(::File.join(path, "metadata.rb")), "license").to_s.casecmp("All Rights Reserved") == 0
       ensure_file_exists(path, "LICENSE")
