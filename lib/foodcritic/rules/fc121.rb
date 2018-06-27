4ab5d6a77c66a87811ca9a24d6fde7fa71ebf689
Tim Smith <tsmith@chef.io>
2018-06-27 16:02:09 -0700

    Chefstyle fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc121.rb
+++ lib/foodcritic/rules/fc121.rb
@@ -1,6 +1,6 @@
 rule "FC121", "Cookbook depends on cookbook where resources are built into Chef 14" do
   tags %w{correctness}
   metadata do |ast, filename|
-    [file_match(filename)] unless (declared_dependencies(ast) & %w(build-essential dmg chef_handler chef_hostname mac_os_x swap sysctl)).empty?
+    [file_match(filename)] unless (declared_dependencies(ast) & %w{build-essential dmg chef_handler chef_hostname mac_os_x swap sysctl}).empty?
   end
 end
