d45eeee67c808dc1bd654fb368f74e4bccb7c61a
Tim Smith <tsmith@chef.io>
2018-06-27 16:14:37 -0700

    Rename this rule
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/foodcritic/rules/fc121.rb
+++ lib/foodcritic/rules/fc121.rb
@@ -1,4 +1,4 @@
-rule "FC121", "Cookbook depends on cookbook where resources are built into Chef 14" do
+rule "FC121", "Cookbook depends on cookbook made obsolete by Chef 14" do
   tags %w{correctness}
   metadata do |ast, filename|
     [file_match(filename)] unless (declared_dependencies(ast) & %w{build-essential dmg chef_handler chef_hostname mac_os_x swap sysctl}).empty?
